b0ebb4ad01adeaf25a719d8cc6e44a6251ad06b3
Lacy Morrow <me@lacymorrow.com>
2024-12-29 03:07:07 -0500

    init

--- src/app/(app)/(bones)/page.tsx
+++ src/app/(app)/(bones)/page.tsx
@@ -10,8 +10,12 @@ export default function Page() {
 				<p className="text-lg">Build apps fast with Shadcn/UI. Features Next.js v15, Tailwind CSS v4, and full AI agent support.</p>
 
 				<div className="flex flex-col md:flex-row gap-md">
-					<Link href={"https://shipkit.io"} className={buttonVariants({ variant: "outline", size: "lg" })}>Learn More</Link>
-					<Link href={"https://github.com/shipkit-io/bones"} className={buttonVariants({ variant: "default", size: "lg" })}>Get Started</Link>
+					<Link href={"https://github.com/shipkit-io/bones"} className={buttonVariants({ variant: "outline", size: "lg" })}>Learn More</Link>
+					<Link href={"https://shipkit.io"} className={buttonVariants({ variant: "default", size: "lg" })}>Get Started</Link>
+				</div>
+
+				<div className="flex flex-col md:flex-row gap-md">
+					Want to see your users logs in real-time? <Link href={"https://log.bones.sh"} className={buttonVariants({ variant: "link", size: "sm" })}>Check out LogBones!</Link>
 				</div>
 			</div>
 		</>
